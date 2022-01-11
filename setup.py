496e1985fb50c3f8492f54ef90386c47e172649a
Artem <a.levchenko@adversa.ai>
2022-01-11 12:54:37 +0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mt4_utils',
+    version="0.0.1",
+    packages=['message', 'serializer'],
+    url='',
+    license='',
+    author='user',
+    author_email='',
+    description=''
+)
