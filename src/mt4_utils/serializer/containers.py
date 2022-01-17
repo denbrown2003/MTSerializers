0472599e510db60af9e5eafa980e09dfc5832865
Artem <a.levchenko@adversa.ai>
2022-01-17 15:31:37 +0400

    Added typings

--- src/mt4_utils/serializer/containers.py
+++ src/mt4_utils/serializer/containers.py
@@ -1,4 +1,4 @@
-from typing import Type, TypeVar, Generic
+from typing import Type, TypeVar, Generic, List, Iterator
 from .base import BaseSerializer
 
 __all__ = [
@@ -19,13 +19,13 @@ class ListSerializer(Generic[T]):
         self.model_full_length = model.size if model.size else model.format_size
         self.model_size = model.format_size
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[T]:
         for idx, i in enumerate(range(0, len(self.buffer), self.model_full_length)):
             model_buff = self.buffer[i:i + self.model_size]
             model = self.model(model_buff)
             yield model
 
-    def get_objects(self):
+    def get_objects(self) -> List[T]:
         return list(self.__iter__())
 
     def __len__(self):
